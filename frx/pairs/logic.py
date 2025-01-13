780c0bf3c3199bfea3cc1626fde663de9a97d4a6
rlatifov <raslatifov@gmail.com>
2025-01-13 09:05:25 +0400

    initial commit

--- frx/pairs/logic.py
+++ frx/pairs/logic.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
-from frx.settings import WORKING_HOURS_START, WORKING_HOURS_END
+from frx.settings import WORKING_HOURS_START, WORKING_HOURS_END, WORKING_WEEK_DAYS_START, WORKING_WEEK_DAYS_END
 
 
 def working_hours():
+    if int(WORKING_WEEK_DAYS_START) <= datetime.now().weekday() + 1 <= int(WORKING_WEEK_DAYS_END):
+        return True
+
     if int(WORKING_HOURS_START) <= datetime.now().hour < int(WORKING_HOURS_END):
         return True
+
     return False
